c3e433b2065ef0e69542e3fa2fbf268b95c08fe4
Simon Darius <simondarius0@gmail.com>
2023-08-08 20:44:23 +0300

    added backend implementation

--- src/Components/Pool.js
+++ src/Components/Pool.js
@@ -9,18 +9,29 @@ const Poll = () => {
     { id: 3, text: 'Toyota ', votes: 0 },
     { id: 4, text: 'Ferrari ', votes: 0 },
   ]);
+  const [userIP, setUserIP] = useState('');
 
+  useEffect(() => {
+    axios.get('https://api64.ipify.org?format=json')
+      .then(response => {
+        setUserIP(response.data.ip);
+      })
+      .catch(error => {
+        console.error('Error getting IP address:', error);
+      });
+  }, []);
   const nrVoturi = async () => {
     try {
-      const response = await axios.get('.http://localhost:5000/votes'); 
+      const response = await axios.get('http://localhost:5000/score_calculator'); 
       const votesData = response.data;
-
-      setOptions((prevOptions) =>
-        prevOptions.map((option) => {
-          const votes = votesData.find((vote) => vote.id === option.id);
-          return { ...option, votes: votes ? votes.votes : 0 };
-        })
-      );
+  
+      const newOptions = Object.entries(votesData).map(([text, votes], id) => ({
+        id: id + 1,
+        text,
+        votes,
+      }));
+  
+      setOptions(newOptions);
     } catch (error) {
       console.error('Error getting votes:', error);
     }
@@ -30,9 +41,9 @@ const Poll = () => {
     nrVoturi(); 
   }, []);
 
-  const handleVote = async (id) => {
+  const handleVote = async (choice) => {
     try {
-      await axios.post('.http://localhost:5000/vote', { id }); 
+      await axios.post('http://localhost:5000/add_scores', { 'update_field':choice,'user_ip':userIP}); 
       nrVoturi(); 
     } catch (error) {
       console.error('Error voting:', error);
@@ -46,7 +57,7 @@ const Poll = () => {
         {options.map((option) => (
           <li key={option.id}>
             {option.text} - votes: {option.votes}
-            <button className='Butone' onClick={() => handleVote(option.id)}>Vote</button>
+            <button className='Butone' onClick={() => handleVote(option.text)}>Vote</button>
           </li>
         ))}
       </ul>
